78339b7c93eda2e99238bd56acc637ab6657237e
Otto Virtanen <oljvirtanen@gmail.com>
2015-12-01 17:37:57 +0100

    Progress in Batch Loader GUI.

--- @Batch_loader_view/initialize_gui.m
+++ @Batch_loader_view/initialize_gui.m
@@ -16,7 +16,7 @@ fig.ToolBar = 'none';
 fig.MenuBar = 'none';
 
 fig.Units = 'pixels';
-fig.Position = [22  271 720  570]; %[left bottom width height]
+fig.Position = [22  271 720  630]; %[left bottom width height]
 fig.Resize = 'off';
 
 obj.gui = fig;
@@ -124,18 +124,140 @@ p_data.Position = [t.Position(1) t.Position(2)-panel_spacer-panel_height panel_w
     btn.Position = [(p_data.Position(3)-btn_width)./2 btn_spacer  btn_width btn_height];       
 
 % Parameter policy panel --------------------------------------------------
-p_policy = uipanel(fig);
-p_policy.Title = 'Parameter update policy';
-p_policy.Position = [t.Position(1)+(panel_spacer+panel_width) t.Position(2)-panel_spacer-panel_height panel_width panel_height];
 
+p_policy = uibuttongroup(fig);
+p_policy.Units = 'pixels';
+p_policy.Title = 'Parameter update policy';
+
+panel_height = 3.* rbtn_spacer + 2.*rbtn_height;
+
+p_policy.Position = [t.Position(1)+(panel_spacer+panel_width) t.Position(2)-panel_spacer-panel_height panel_width panel_height];
+p_policy_width = p_policy.Position(3);
+p_policy_height = p_policy.Position(4);
+
+
+% Pushbuttons
+       
+            rbtn_width = 0.9.*panel_width;
+        
+            r1 = uicontrol(p_policy,'Style','radiobutton');
+            r1.Units = 'pixels';
+            r1.String = 'Always keep updated';
+            r1.Position = [(p_policy_width - rbtn_width)./2 p_policy_height-(rbtn_spacer+rbtn_height) rbtn_width rbtn_height];
+            
+            r2 = uicontrol(p_policy,'Style','radiobutton');
+            r2.Units = 'pixels';
+            r2.String = 'Update only after fit';
+            r2.Position = [(p_policy_width - rbtn_width)./2 p_policy_height-2.*(rbtn_spacer+rbtn_height) rbtn_width rbtn_height];
+
+
+% Batch fit panel ---------------------------------------------------------
 p_batch = uipanel(fig);
+p_batch.Units = 'pixels';
 p_batch.Title = 'Batch fitting';
-p_batch.Position = [t.Position(1)+2.*(panel_spacer+panel_width) t.Position(2)-panel_spacer-panel_height panel_width panel_height];
+
+% Spacers and heights
+
+fitgroup_height = 2.*(rbtn_spacer+rbtn_height)+rbtn_spacer;
+fitoptions_height = 3.*(rbtn_spacer+rbtn_height)+rbtn_spacer;
+
+p_batch_height = fitgroup_height + fitoptions_height +2.* b_group_spacer + +2.* btn_spacer + btn_height;
+
+p_batch.Position = [t.Position(1)+2.*(panel_spacer+panel_width) t.Position(2)-(panel_spacer+p_batch_height) panel_width p_batch_height];
+
+% Pushbutton group for choosing data for fitting
+
+        bgroup = uibuttongroup(p_batch);
+        bgroup.Title = 'Fit';
+        bgroup.Units = 'pixels';
+        
+        bgroup_width = 0.9.*p_batch.Position(3);
+        
+        
+        bgroup.Position = [(p_batch.Position(3)-bgroup_width)./2 bgroup.Position(4)-(b_group_spacer+fitgroup_height) bgroup_width fitgroup_height];
+
+        % Pushbuttons
+       
+            rbtn_width = 0.9.*bgroup_width;
+        
+            r1 = uicontrol(bgroup,'Style','radiobutton');
+            r1.Units = 'pixels';
+            r1.String = 'All';
+            r1.Position = [(bgroup_width - rbtn_width)./2 fitgroup_height-(rbtn_spacer+rbtn_height) rbtn_width rbtn_height];
+            
+            r2 = uicontrol(bgroup,'Style','radiobutton');
+            r2.Units = 'pixels';
+            r2.String = 'Only selected';
+            r2.Position = [(bgroup_width - rbtn_width)./2 fitgroup_height-2.*(rbtn_spacer+rbtn_height) rbtn_width rbtn_height];
+            
+% Pushbutton group for selecting fitting mode
+
+        bgroup = uibuttongroup(p_batch);
+        bgroup.Title = 'Parameter vector p';
+        bgroup.Units = 'pixels';
+        
+        bgroup_width = 0.9.*p_batch.Position(3);
+
+        bgroup.Position = [(p_batch.Position(3)-bgroup_width)./2 bgroup.Position(4)-(2.*b_group_spacer+fitgroup_height+fitoptions_height) bgroup_width fitoptions_height];
+
+        % Pushbuttons
+       
+            rbtn_width = 0.9.*bgroup_width;
+        
+            r1 = uicontrol(bgroup,'Style','radiobutton');
+            r1.Units = 'pixels';
+            r1.String = 'Use original p';
+            r1.Position = [(bgroup_width - rbtn_width)./2 fitoptions_height-(rbtn_spacer+rbtn_height) rbtn_width rbtn_height];
+            
+            r2 = uicontrol(bgroup,'Style','radiobutton');
+            r2.Units = 'pixels';
+            r2.String = 'Use active p';
+            r2.Position = [(bgroup_width - rbtn_width)./2 fitoptions_height-2.*(rbtn_spacer+rbtn_height) rbtn_width rbtn_height];
+
+            r3 = uicontrol(bgroup,'Style','radiobutton');
+            r3.Units = 'pixels';
+            r3.String = 'Propagate active p';
+            r3.Position = [(bgroup_width - rbtn_width)./2 fitoptions_height-3.*(rbtn_spacer+rbtn_height) rbtn_width rbtn_height];
+
+    % Fit data button
+    btn = uicontrol(p_batch,'Style','pushbutton');
+    btn.String = 'Fit';
+
+    btn.Units = 'pixels';
+
+    btn_width = 0.9.*p_batch.Position(3);
+    
+    btn.Position = [(p_batch.Position(3)-btn_width)./2 p_batch.Position(4)-(2.*b_group_spacer+fitgroup_height+fitoptions_height+btn_spacer+btn_height)  btn_width btn_height];
+
+% Save fit panel ---------------------------------------------------------
 
 p_save = uipanel(fig);
+p_save.Units = 'pixels';
 p_save.Title = 'Saving';
+
+chckbox_spacer = 5;
+
+panel_height = 3.* chckbox_spacer + 3.*rbtn_height+2.*btn_spacer+btn_height;
+
 p_save.Position = [t.Position(1)+3.*(panel_spacer+panel_width) t.Position(2)-panel_spacer-panel_height panel_width panel_height];
 
+p_save_width = p_save.Position(3);
+p_save_height = p_save.Position(4);
+
+c1 = uicontrol(p_save,'Style','checkbox');
+c1.Units = 'pixels';
+c1.String = 'Automatic saving';
+c1.Position = [(p_save_width  - rbtn_width)./2 p_save_height-(1.*(chckbox_spacer+rbtn_height)+chckbox_spacer) rbtn_width rbtn_height];
+
+c2 = uicontrol(p_save,'Style','checkbox');
+c2.Units = 'pixels';
+c2.String = 'Export to p table';
+c2.Position = [(p_save_width  - rbtn_width)./2 p_save_height-(2.*(chckbox_spacer+rbtn_height)+chckbox_spacer) rbtn_width rbtn_height];
+
+c3 = uicontrol(p_save,'Style','checkbox');
+c3.Units = 'pixels';
+c3.String = 'Export as text';
+c3.Position = [(p_save_width  - rbtn_width)./2 p_save_height-(3.*(chckbox_spacer+rbtn_height)+chckbox_spacer) rbtn_width rbtn_height];
 
 % fig.Position(4) = t_spacer + t_height + t_spacer + max([p_data.Position(4) p_policy.Position(4) p_batch.Position(4) p_save.Position(4)]) + t_spacer;
 
