7f260e30c3887f81957fb4bacbae31db133c751a
Otto Virtanen <oljvirtanen@gmail.com>
2015-12-02 22:38:31 +0100

    Added copyright.

--- @Batch_loader_view/update_table.m
+++ @Batch_loader_view/update_table.m
@@ -1,6 +1,9 @@
 function update_table(obj)
 %UPDATE_TABLE Updates the Batch_loader GUI data table
 
+% Copyright (c) 2015, Otto Virtanen
+% All rights reserved.
+
 [d,ci] = Batch_loader.data_nodes_to_table(obj.view.model.bl.nodes);
 obj.file_table.Data = d;
 obj.file_table.RowName = ci;
