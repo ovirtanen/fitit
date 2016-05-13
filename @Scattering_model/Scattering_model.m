f1c51947694f6d5bc069aa62f5f3aea667639dee
Otto Virtanen <oljvirtanen@gmail.com>
2016-05-13 16:15:07 +0200

    Hot fix to make CS dumbbell model work, removed triplet model for now.

--- @Scattering_model/Scattering_model.m
+++ @Scattering_model/Scattering_model.m
@@ -11,7 +11,7 @@ classdef Scattering_model < handle
                                             'Free Profile Model, 1st deriv. SN';
                                             'Free Profile Model, 2nd deriv. SN';
                                             'Core-shell dumbbell aggregation model';
-                                            'Core-shell triplet aggregation model';
+                                            %'Core-shell triplet aggregation model';
                                             'SANS Polymer Particle Model'
                                             'Stieger Microgel Model';
                                             'Numerical Microgel Model';
@@ -24,8 +24,8 @@ classdef Scattering_model < handle
                                             @(d)SM_Free_profile(d,20,-1);
                                             @(d)SM_Free_profile(d,20,1);
                                             @(d)SM_Free_profile(d,20,2);
-                                            @(d)SM_CS_dumbbell(d,obj.gpu_enabled_global,obj.par_enabled_global);
-                                            @(d)SM_CS_triplets(d,obj.gpu_enabled_global,obj.par_enabled_global);
+                                            @(d)SM_CS_dumbbell(d,false,false);
+                                            %@(d)SM_CS_triplets(d,false,false);
                                             @(d)SM_SANS_HS(d);
                                             @(d)SM_Stieger(d);
                                             @(d)SM_MG_numerical(d);
