b3d7b52a1a3988c293eed79a77bd36c38a111393
Otto Virtanen <oljvirtanen@gmail.com>
2016-05-13 16:16:23 +0200

    Increment version number.

--- @View/View.m
+++ @View/View.m
@@ -71,7 +71,7 @@ classdef View < handle
         
         function obj = View(c,m)
             
-            obj.version = '1.2.3';
+            obj.version = '1.2.3.1';
             
             obj.graphics_root = groot();
             
