d233aedad1f4be45c05694178f0a668f6d1841a8
Otto Virtanen <oljvirtanen@gmail.com>
2016-06-30 17:58:41 +0200

    Fix to the radial profile of fuzzy core-shell.

--- @SM_Fuzzy_core_shell/radial_profile.m
+++ @SM_Fuzzy_core_shell/radial_profile.m
@@ -28,29 +28,34 @@ sigma_core = sigma_core_frac * (r_tot-r_box)/2;
 ws = ws_frac .* (r_tot - r_box - 2.*sigma_core);
 sigma_shell = (r_tot - (r_box + ws + 2.*sigma_core))/2;
 
-rprf = linspace(0,r_tot,100);
+rprf = linspace(0,1.01.*r_tot,100);
 
 % Create filters for the pairwise defined function
 f_core = rprf <= r_box;
 f_fuzzy_core_in = rprf > r_box & rprf <= r_box + sigma_core;
 f_fuzzy_core_out = rprf > r_box + sigma_core & rprf <= r_box + 2* sigma_core;
+f_fuzzy_core_zero = rprf > r_box + 2* sigma_core;
 
 r_box_shell = r_box + 2* sigma_core + ws;
 f_shell = rprf <= r_box_shell;
 f_fuzzy_shell_in = rprf > r_box_shell & rprf <= r_box_shell + sigma_shell;
 f_fuzzy_shell_out = rprf > r_box_shell + sigma_shell & rprf <= r_box_shell + 2* sigma_shell;
+f_fuzzy_shell_zero = rprf > r_tot;
 
 % Calculate profile of the core particle
 prf_c = zeros(size(rprf));
 prf_c(f_core) = 1;
 prf_c(f_fuzzy_core_in) = 1 - 0.5.*((rprf(f_fuzzy_core_in)-(r_box + sigma_core)) + sigma_core).^2./sigma_core.^2;
 prf_c(f_fuzzy_core_out) = 0.5.*(((r_box + sigma_core) - rprf(f_fuzzy_core_out)) + sigma_core).^2./sigma_core.^2;
+prf_c(f_fuzzy_core_zero) = 0;
+
 
 % Calculate profile of the shell partiicle
 prf_s = zeros(size(rprf));
 prf_s(f_shell) = 1;
 prf_s(f_fuzzy_shell_in) = 1 - 0.5.*((rprf(f_fuzzy_shell_in) - (r_box_shell + sigma_shell)) + sigma_shell).^2./sigma_shell.^2;
 prf_s(f_fuzzy_shell_out) = 0.5.*(((r_box_shell + sigma_shell) - rprf(f_fuzzy_shell_out)) + sigma_shell).^2./sigma_shell.^2;
+prf_s(f_fuzzy_shell_zero) = 0;
 
 % Return overall profile
 prf = vfs .* prf_s + (vfc - vfs) .* prf_c;
